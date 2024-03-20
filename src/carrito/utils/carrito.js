f05e7dc6d65dd53634c9a0e12b3dd26c13ae4d66
ingeniero 10 <ingeniero.desarrollo10@comersantander.com>
2024-03-20 09:09:38 -0500

    hello word

--- src/carrito/utils/carrito.js
+++ src/carrito/utils/carrito.js
@@ -1,4 +1,4 @@
-export function generateRandomCode(length: number): string {
+export function generateRandomCode(length)  {
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let randomCode = '';
